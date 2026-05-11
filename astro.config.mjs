ca0fd9798382dc5e031f3275c1feefb37674a758
tatsukoizumi <koizumitattsuu@gmail.com>
2026-05-11 11:28:03 +0900

    fix config to show image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
+  adapter: cloudflare({ imageService: "compile" }),
   output: "static",
 });
